96e952e721cdde05df6b19555327437ca00c6e91
William <williamzak0@gmail.com>
2020-09-23 20:38:24 +0000

    added two new funcs

--- play.js
+++ play.js
@@ -1,3 +1,22 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = () => {
+    stdin.on('data', (data) => {
+      if (data == '\u0003') {
+        process.exit();
+      }
+    })
+  };
+  handleUserInput();
+  return stdin;
+}
+
+setupInput();
